c932fe7963fba1f5556b19e2677e180cd17193d8
hayoung0402 <hy950402@gmail.com>
2021-10-03 21:30:48 +0900

    빗물 코드

---
+++ jhy/210930/빗물.py
@@ -0,0 +1,22 @@
+import sys
+
+h, w = map(int, sys.stdin.readline().split())
+
+array = list(map(int, sys.stdin.readline().split()))
+
+height = min(array[0], max(array[1:]))
+
+result = 0
+
+for i in range(w - 1):
+    gap = height - array[i]
+
+    if gap > 0:
+        
+        result += gap
+    else:
+        if i == w - 1:
+            break
+        height = min(array[i], max(array[i + 1:]))
+
+print(result)

d082f50f889d00475efae93a165365e1fe700fd1
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-11-21 13:35:16 +0900

    boj 16987 계계치

---
+++ nyc/211116/baekjoon_16987_계계치.py
@@ -0,0 +1,41 @@
+import sys
+
+
+def solution(cur):
+    global eggs
+    global answer
+
+    count = 0
+    for egg in eggs:
+        if egg[0] <= 0:
+            count += 1
+    answer = max(answer, count)
+
+    if cur == len(eggs):
+        return
+
+    if eggs[cur][0] <= 0:
+        solution(cur+1)
+        return
+
+    flag = False
+    for i in range(len(eggs)):
+        if i == cur or eggs[i][0] <= 0:
+            continue
+        flag = True
+        eggs[cur][0] -= eggs[i][1]
+        eggs[i][0] -= eggs[cur][1]
+        solution(cur+1)
+        eggs[cur][0] += eggs[i][1]
+        eggs[i][0] += eggs[cur][1]
+
+    if not flag:
+        solution(cur+1)
+
+
+if __name__ == "__main__":
+    N = int(sys.stdin.readline().rstrip())
+    answer = 0
+    eggs = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    solution(0)
+    print(answer)

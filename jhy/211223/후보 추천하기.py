49f9e9075a738056bc20b7b6287b522ab6b73de0
hayoung0402 <hy950402@gmail.com>
2021-12-28 13:47:01 +0900

    후보 추천하기

---
+++ jhy/211223/후보 추천하기.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+
+def update(candidate):
+    if len(candidates) == n:
+        if candidate not in candidates.keys():
+            idx = list(candidates.values()).index(min(candidates.values()))
+            del candidates[list(candidates.keys())[idx]]
+        
+    if candidate not in candidates.keys():
+            candidates[candidate] = 1
+    else:
+        candidates[candidate] += 1
+    
+    return list(candidates.keys())
+
+
+def solution(n):
+    for _ in range(recommend):
+        candidate = number.popleft()
+        result = update(candidate)
+    result.sort()
+    for i in range(len(result)):
+        print(result[i], end=" ")
+
+n = int(input())
+recommend = int(input())
+number = deque(map(int, input().split()))
+candidates = {}
+
+solution(n)
